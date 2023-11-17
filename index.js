a5037bf86508133ced5231bbbf2c12d368e714e2
ftrianaa <fitriana0116@gmail.com>
2023-11-17 10:24:31 +0700

    true

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(
      bodyParser.urlencoded({
-          extended: false,
+          extended: true
      })
 )
 app.get('/', (request, response) => {
