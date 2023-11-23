04407767108e724b32a492e5b766c51e7535f2cc
ftrianaa <fitriana0116@gmail.com>
2023-11-24 06:15:53 +0700

    delete

--- services/experiencesService.js
+++ services/experiencesService.js
@@ -1,6 +1,6 @@
 module.exports = exports = (app, pool) => {
      app.get('/api/experiences', (request, response) => {
-          const query = `SELECT * FROM experiences ORDER BY "createdAt"`
+          const query = `SELECT * FROM experiences ORDER BY "createdAt" DESC`
 
           pool.query(query, (error, result) => {
                if (error) {
@@ -17,7 +17,6 @@ module.exports = exports = (app, pool) => {
           })
      })
      app.post('/api/create/experience', (request, response) => {
-
           const { title, desc, link, preview, tags } = request.body
           const query = `INSERT INTO experiences("title", "desc", "link", "preview", "tags", "createdAt")
                VALUES ('${title}', '${desc}', '${link}', '${preview}', '{Next.js}', now())`;
@@ -32,6 +31,24 @@ module.exports = exports = (app, pool) => {
                     response.send(200, {
                          success: true,
                          data: `Data experience has been saved!`
+                    })
+               }
+          })
+     })
+     app.delete('/api/delete/experience/:id', (request, response) => {
+          const { id } = request.params
+
+          const query = `DELETE FROM experiences WHERE "experiencesId" = '${id}'`
+          pool.query((query), (error, result) => {
+               if (error) {
+                    response.send(400, {
+                         success: false,
+                         data: error
+                    })
+               } else {
+                    response.send(200, {
+                         success: true,
+                         data: `Data experience has been deleted!`
                     })
                }
           })
